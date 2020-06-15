32fbc209a772075dcfe418fb66cde592157c282a
Jason Cornelio <jasoncorneliog@gmail.com>
2020-06-14 21:53:46 -0400

    Updates

---
+++ precache-manifest.33f22644aecb12550db18a8bd338e95d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea6cedb41b5c7a9d161ae731fdc7161",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "803cd770caa33fe8e158",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/portfolio/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "803cd770caa33fe8e158",
+    "url": "/portfolio/static/js/main.0d2a6465.chunk.js"
+  },
+  {
+    "revision": "0c18a05f1fb6137df5ad",
+    "url": "/portfolio/static/js/runtime-main.24d195ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
